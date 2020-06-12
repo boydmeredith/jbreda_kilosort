208da60b850cd711bb7e1832403d67f6137a0a90
jess-breda <jbreda@princeton.edu>
2020-06-12 15:50:06 -0400

    Updated tetrode_32_mdatobin.m to work on current .mda storage structure in bucket & windows

---
+++ pre_kilosort_functions/template.m
@@ -0,0 +1,21 @@
+
+% ---------------------
+% written by 
+% purpose is to 
+%
+%
+% TODO:
+% - 
+%
+% INPUT PARAMETERS:
+% - 
+% 
+% OPTIONAL PARAMETERS:
+% - 
+% 
+% RETURNS:
+% - 
+% 
+% = EXAMPLE CALLS:
+% - 
+% ---------------------
