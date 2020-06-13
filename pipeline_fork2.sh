a9f1e2e7f1be8751e478d7da03387eba6486ddef
emilyjanedennis <emilyjanedennis@gmail.com>
2020-06-13 16:18:09 -0400

    debugged in spock - now step 1 works with any input but step2 is giving an error that config not root node

--- pipeline_fork2.sh
+++ pipeline_fork2.sh
@@ -4,16 +4,17 @@ stringinputfromuser=$1
 echo $stringinputfromuser
 lengthofstring=${#stringinputfromuser}
 echo $lengthofstring
-session={$stringinputfromuser:0:lengthofstring-4}
+
+session=${stringinputfromuser:0:lengthofstring-4}
 echo $session
-typeoffile={$stringinputfromuser:lengthofstring-3:3}
+typeoffile=${stringinputfromuser:lengthofstring-4:4}
 echo $typeoffile
 
 echo "Processing Session $session"
 
 
-if [ typeoffile = ".dat" ]; then
-	echo "Step stringinputfromuser: Creating rec file from dat file"
+if typeoffile=".dat"; then
+	echo "Step 1: Creating rec file from dat file"
 	./sdtorec -sd $stringinputfromuser -numchan 128 -mergeconf 128_Tetrodes_Sensors_CustomRF.trodesconf
 #	rm ${session}.dat
 else
