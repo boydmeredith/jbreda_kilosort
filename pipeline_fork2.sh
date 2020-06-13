f67c68ac15ee4f1816396ed9e05dcbaf58778ed1
emilyjanedennis <emilyjanedennis@gmail.com>
2020-06-13 16:13:16 -0400

    debugging on spock: first added 128 tetrode.conf file, also in pipeline_fork2 I was having issues with inputs so I changed it from a number to a saved variable

--- pipeline_fork2.sh
+++ pipeline_fork2.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-lengthofstring=${"#1"}
-session={$1:0:lengthofstring-4}
-typeoffile={$1:lengthofstring-3:3}
+stringinputfromuser=$1
+echo $stringinputfromuser
+lengthofstring=${#stringinputfromuser}
+echo $lengthofstring
+session={$stringinputfromuser:0:lengthofstring-4}
+echo $session
+typeoffile={$stringinputfromuser:lengthofstring-3:3}
+echo $typeoffile
 
 echo "Processing Session $session"
 
 
 if [ typeoffile = ".dat" ]; then
-	echo "Step 1: Creating rec file from dat file"
-	./sdtorec -sd $1 -numchan 128 -mergeconf 128_Tetrodes_Sensors_CustomRF.trodesconf
+	echo "Step stringinputfromuser: Creating rec file from dat file"
+	./sdtorec -sd $stringinputfromuser -numchan 128 -mergeconf 128_Tetrodes_Sensors_CustomRF.trodesconf
 #	rm ${session}.dat
 else
 	echo "Skipping step 1: creation rec file"
 fi
 
 	echo "Step 2: Creating mda files from rec file"
-	./exportdio -rec $1
-	./exportmda -rec $1
+	./exportdio -rec $stringinputfromuser
+	./exportmda -rec $stringinputfromuser
 #	mv ${session}.rec recs
 #	mv ${session}.DIO/* ${session}.mda
 #	rmdir ${session}.DIO
