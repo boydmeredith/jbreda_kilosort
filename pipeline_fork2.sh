03b1f97875fe09f7a0a63efa6e6e4396079f4a95
emilyjanedennis <emilyjanedennis@gmail.com>
2020-06-13 15:06:36 -0400

    in pipeline_fork2 I changed the inputs used from 2 to 1 - this allows the user to input a filename and the script changes it as needed doing whatever steps make sense depending on if it's a dat or rec file

--- pipeline_fork2.sh
+++ pipeline_fork2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-session=$1
-
-step=$2
+lengthofstring=${"#1"}
+session={$1:0:lengthofstring-4}
+typeoffile={$1:lengthofstring-3:3}
 
 echo "Processing Session $session"
 
 
-if [[ $step < 2 ]]; then
+if [ typeoffile = ".dat" ]; then
 	echo "Step 1: Creating rec file from dat file"
 	sdtorec -sd ${session}.dat -numchan 128 -mergeconf 128_Tetrodes_Sensors_CustomRF.trodesconf
 #	rm ${session}.dat
