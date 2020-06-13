3d3d610390626346a99af73d42c31b92f84148d4
emilyjanedennis <emilyjanedennis@gmail.com>
2020-06-13 15:21:01 -0400

    added ./ to binary calls so it doesn't matter where they're stored

--- pipeline_fork2.sh
+++ pipeline_fork2.sh
@@ -9,15 +9,15 @@ echo "Processing Session $session"
 
 if [ typeoffile = ".dat" ]; then
 	echo "Step 1: Creating rec file from dat file"
-	sdtorec -sd $1 -numchan 128 -mergeconf 128_Tetrodes_Sensors_CustomRF.trodesconf
+	./sdtorec -sd $1 -numchan 128 -mergeconf 128_Tetrodes_Sensors_CustomRF.trodesconf
 #	rm ${session}.dat
 else
 	echo "Skipping step 1: creation rec file"
 fi
 
 	echo "Step 2: Creating mda files from rec file"
-	exportdio -rec $1
-	exportmda -rec $1
+	./exportdio -rec $1
+	./exportmda -rec $1
 #	mv ${session}.rec recs
 #	mv ${session}.DIO/* ${session}.mda
 #	rmdir ${session}.DIO
