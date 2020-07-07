f1c3d53319dbb24499b80dd5b03cf5193e490055
jess-breda <jbreda19@gmail.com>
2020-07-07 13:26:59 -0400

    initial mda to bin slurm script

---
+++ mda_to_bin_slurm.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#SBATCH --nodes=1                            # node count
+#SBATCH -o /jukebox/scratch/jbreda/ephys/ephys_W122.out  # where to save the output files of the job
+#SBATCH -e /jukebox/scratch/jbreda/ephys/ephys_W122.err  # where to save the error files of the job
+#SBATCH -t 840                               # 14 hour time limit
+#SBATCH --mem=32000 # 32GB of RAM
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jbreda@princeton.edu
+#SBATCH --cpus-per-task=11                   # 11 cores requested
+#SBATCH --partition=Brody                    # run on brodylab parition
+
+# pick up from where you left of in previous script & move into repo
+input_folder="/jukebox/scratch/jbreda/ephys/data_sdc_20190902_145505_pipeline"
+cd $input_folder
+repo="Brody_Lab_Ephys"
+cd $repo
+
+# grab the current jobid and print it
+jobid=$SLURM_JOB_ID
+echo $jobid
+
+# open matlab etc
+module load matlab/R2019b5
+
+# call kilosortpipelineforcluster
+	matlab -nosplash -nodisplay -nodesktop -r "kilosortpipelineforcluster('${input_folder}','${repo}', '${job_id}');exit;"
+
+
+# echo "matlab worked"
