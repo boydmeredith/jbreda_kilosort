de309f2203850793e8367a8eefc854ace4603ca6
jess-breda <jbreda19@gmail.com>
2020-07-07 21:12:24 -0400

    updated RAM request to run mda_to_bin

--- mda_to_bin_slurm.sh
+++ mda_to_bin_slurm.sh
@@ -3,7 +3,7 @@
 #SBATCH -o /jukebox/scratch/jbreda/ephys/mdatobin.out  # where to save the output files of the job
 #SBATCH -e /jukebox/scratch/jbreda/ephys/mdatobin.err  # where to save the error files of the job
 #SBATCH -t 840                               # 14 hour time limit
-#SBATCH --mem=32000 # 32GB of RAM
+#SBATCH --mem=64000 # 32GB of RAM
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jbreda@princeton.edu
 #SBATCH --cpus-per-task=11                   # 11 cores requested
