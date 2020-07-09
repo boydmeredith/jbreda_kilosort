08a260bdf55caa0315da21f4fd6461b45039b0c7
jess-breda <jbreda19@gmail.com>
2020-07-08 20:36:29 -0400

    updated memory needed for W122 files, job 19523713

--- mda_to_bin_slurm.sh
+++ mda_to_bin_slurm.sh
@@ -3,7 +3,7 @@
 #SBATCH -o /jukebox/scratch/jbreda/ephys/W122/mdatobin.out  # where to save the output files of the job
 #SBATCH -e /jukebox/scratch/jbreda/ephys/W122/mdatobin.err  # where to save the error files of the job
 #SBATCH -t 840                               # 14 hour time limit
-#SBATCH --mem=64000 # 64GB of RAM
+#SBATCH --mem=128000                         # 128 GB of RAM
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jbreda@princeton.edu
 #SBATCH --cpus-per-task=11                   # 11 cores requested
