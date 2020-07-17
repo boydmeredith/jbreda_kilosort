b04992dafb8c4648c8955ef7180725d5c5453731
jess-breda <jbreda19@gmail.com>
2020-07-16 20:40:34 -0400

    updated kilosort_preprocess with variable naming and documentation

--- kilosort_preprocess.m
+++ kilosort_preprocess.m
@@ -15,7 +15,7 @@ function kilosort_preprocess(varargin)
 % - come back after some kilosort testing and add a if/else statement for
 % if we should be saving the new binary files with the bad sections
 % removed, zerod, or interpd
-% - put each file into a folder with it's name (kilosort takes single .bin)
+% 
 %
 % INPUT PARAMETERS:
 % - none needed - it should be able to be run from a local folder
@@ -33,14 +33,16 @@ function kilosort_preprocess(varargin)
 % = EXAMPLE CALLS:
 % - 
 % ---------------------
-%%
+%% inputs
 if isempty(varargin)
     homedirectory=pwd;
     chan=32;
     ops.fs     = 32000;    
-    ops.fshigh = 300;        
-    %if user provides input, check that it is a string for a directory
+    ops.fshigh = 300;
+%     threshold = 0.5
+%     window = 10000
 
+%if user provides input, check that it is a string for a directory
 elseif length(varargin)==1
     if isfolder(varargin{1})
         homedirectory=pwd;
@@ -52,32 +54,54 @@ elseif length(varargin)==1
     chan=32;    
     ops.fs     = 32000;    
     ops.fshigh = 300;
+%     threshold = 0.5
+%     window = 10000
+    
 elseif length(varargin)==2
     chan = varargin{2};
     ops.fs     = 32000;    
     ops.fshigh = 300;
+%     threshold = 0.5
+%     window = 10000
+    
 elseif varargin == 3
     chan=varargin{2};
     ops.fs = varargin{3};
     ops.fshigh=300;
-else
-    ops.fs=varargin{3};
-    ops.fshigh=varargin{4};
+%     threshold = 0.5
+%     window = 10000
+    
+elseif varagin == 4
+    chan=varargin{2};
+    ops.fs = varargin{3};
+    ops.fshigh =varagin{4}
+%     threshold = 0.5
+%     window = 10000
+    
+% elseif varagin == 5
+%     chan=varargin{2};
+%     ops.fs = varargin{3};
+%     ops.fshigh = varagin{4}
+%     threshold = varagin{5}
+%     window = 10000
+% else
+%     chan=varargin{2};
+%     ops.fs = varargin{3};
+%     ops.fshigh = varagin{4}
+%     threshold = varagin{5}
+%     window = varagin{6}
+    
 end
+%% filter & initialization
 
-    % make a filter for the data based on ops inputs/defaults
-    [b1, a1] = butter(3, ops.fshigh/(ops.fs/2), 'high'); 
-    % butterworth filter with only 3 nodes (otherwise it's unstable for float32)
-    % Wn (ops.fshigh/ops.fs) is the cutoff frequency, and must be between 0.0 and 1.0. 1.0 is half the
-    % sample rate
-    % high means it's a highpass filter
+% make a filter for the data based on ops inputs/defaults 
+% butterworth filter with only 3 nodes (otherwise it's unstable for float32)
+% Wn (ops.fshigh/ops.fs) is the cutoff frequency, and must be between 0.0 and 1.0. 1.0 is half the
+% sample rate
+% high means it's a highpass filter
+%outputs filter coefficients b1 (numerator) and a1 (denominator)
+[b1, a1] = butter(3, ops.fshigh/(ops.fs/2), 'high');
     
-    % bessel filter options
-    
-    %[b1, a1] = besself(1, [600 6000], 'bandpass')
-    
-    %outputs filter coefficients b1 (numerator) and a1 (denominator)
-   
 % make list of files to process
 listofbinaryfiles=dir('*.bin');
 %make empty fftosave
@@ -90,13 +114,13 @@ else
     delim='/';
 end
 
-% for optimizing filter
+% for optimizing filter (TODO: add this to function inputs)
 threshold = 0.5
 window = 10000
-jobid = randi(100)
+jobid = randi(1000)
 
 
-
+%% loop over binary files
 for i = 1:length(listofbinaryfiles)
 
     % first, open the binary file to read
@@ -106,10 +130,15 @@ for i = 1:length(listofbinaryfiles)
     
     % next, name and open a new binary file to write to, and put it in it's
     % own folder
-    mkdir(fullfile(homedirectory, delim, sprintf('%s_T%s_W%s_forkilosort_%s',fname(1:end-4), num2str(threshold*10), num2str(window), num2str(jobid))))
-    kilosortbinfolder = [homedirectory, delim, sprintf('%s_T%s_W%s_forkilosort_%s',fname(1:end-4), num2str(threshold*10), num2str(window), num2str(jobid))]
+    mkdir(fullfile(homedirectory, delim, sprintf('%s_%s_T%s_W%s_forkilosort%s',num2str(jobid),
+    fname(1:end-4), num2str(threshold*10), num2str(window))))
+    
+    kilosortbinfolder = [homedirectory, delim, sprintf('%s_%s_T%s_W%s_forkilosort',num2str(jobid),
+    fname(1:end-4), num2str(threshold*10), num2str(window))]
+    
     addpath(kilosortbinfolder)
     cd(kilosortbinfolder)
+    
     fidw = fopen(sprintf('%s_T%s_W%s_forkilosort.bin',fname(1:end-4), num2str(threshold*10), num2str(window)), 'w');
 
     while 1
@@ -125,21 +154,8 @@ for i = 1:length(listofbinaryfiles)
         % transpose
         dataRAW = dataRAW';
         % divide by 1000 because the filter prefers that
-        dataRAW = double(dataRAW)/1000;
-        
-        
-%         % CAR FILTER
-%         % mean center and add CAR filter- from GPUfilter.m
-%         % subtract the mean from each channel
-%         dataRAW_mean = dataRAW - mean(dataRAW, 1); % subtract mean of each channel
-% 
-%         % CAR, common average referencing by median
-%        
-%         dataRAW_CAR = dataRAW_mean - median(dataRAW_mean, 2); % subtract median across channels
-      
-        
-       
-        
+        dataRAW = double(dataRAW)/1000
+               
         % apply the filter
         datr = filtfilt(b1, a1, dataRAW);
         dataFILT = datr; %renaming now so I can plot later without overwriting
@@ -148,13 +164,16 @@ for i = 1:length(listofbinaryfiles)
         % remove
             % first we want absolute values, so we square datr, take the means of each
             % row (channel), and then take the square root
-        ff = mean(datr.^2, 2).^.5; 
-            % for the rows where ff > 0.5 (ie, good channels), use a window of every 2000 data
-            % points and if the moving average is above 0.5
-        ff1 = movmean(double(ff>threshold), window);
-        %ff0 = double(ff>1)
+        dataABSVAL = mean(datr.^2, 2).^.5; 
+            % create a binary mask where dataABSVAL > threshold
+            % then, take moving mean of mask with window size
+        mask1MEAN = movmean(double(dataABSVAL>threshold), window);
+        
         % binary mask 'signal' = 1, 'noise' = 0
-        ff2=ff1<.00001;
+        mask2= mask1MEAN < 0.00001;
+        
+        % mask data & set noise to 0
+        dataMASK = datr .* ff2;
         
         % TODO add if statement once we have a favorite output for default
 
@@ -163,14 +182,6 @@ for i = 1:length(listofbinaryfiles)
 %         datr=datr(ff2, :);
 %         newff=~ff2;
 %         fftosave=[fftosave newff];
-
-%         save with zeroed out
-%        datr(~ff2, :) = 0;
-        
-
-        % using matrix multiplication because it makes more sense to me (JB)
-        dataMASK = datr .* ff2;
-        
         %save as interp'd
 %        TODO pull out good/bad times, interp end of good blocks to start
 %        of next good block, steal from rmartifacts
@@ -181,16 +192,11 @@ for i = 1:length(listofbinaryfiles)
         dat16 = int16(1000*dataMASK');
         fwrite(fidw, dat16, 'int16');
     end
-   
-
-
 
 fclose(fid);
 fclose(fidw);
 
-%% plots
-%if running as 'remove noise' mode
-%save([homedirectory,delim,fname, '_timemask.mat'],fftosave)
+%% plots for debugging
 
 %  figure(200);subplot(5,1,1);plot(dataRAW(:,2))
 %         title('data','fontsize',14,'fontweight','bold')
@@ -228,11 +234,7 @@ fclose(fidw);
 % legend('butter data', 'abs val data'); title(test_chan);
 
 % playing with window sizes
-% ff_1000 = movmean(double(ff>1), 1000);
-% ff_2000 = movmean(double(ff>1), 2000);
-% ff_3000 = movmean(double(ff>1), 3000);
-% ff_4000 = movmean(double(ff>1), 4000);
-% ff_10000 = movmean(double(ff>1), 10000);
+
 % figure(3); subplot(5,1,1); plot(ff_1000); title('window: 1000'); ylim([0, 0.6]);
 % subplot(5,1,2); plot(ff_2000); title('window: 2000');ylim([0, 0.6]);
 % subplot(5,1,3); plot(ff_3000); title('window: 3000');ylim([0, 0.6]);
@@ -242,12 +244,12 @@ fclose(fidw);
 
 % playing with threshold
 
-% ff_10 = movmean(double(ff>1), 2000); % should be 10 but 
-% ff_5 = movmean(double(ff>0.5), 2000);
-% ff_7 = movmean(double(ff>0.75), 2000);
-% ff_4 = movmean(double(ff>0.4), 2000);
-% ff_3 = movmean(double(ff>0.3), 2000);
-% ff_2 = movmean(double(ff>0.2), 2000);
+% ff_10 = movmean(double(dataABSVAL>1), 2000); % should be 10 but 
+% ff_5 = movmean(double(dataABSVAL>0.5), 2000);
+% ff_7 = movmean(double(dataABSVAL>0.75), 2000);
+% ff_4 = movmean(double(dataABSVAL>0.4), 2000);
+% ff_3 = movmean(double(dataABSVAL>0.3), 2000);
+% ff_2 = movmean(double(dataABSVAL>0.2), 2000);
 % 
 % ffs = [ff_10, ff_5, ff_7, ff_4, ff_3, ff_2];
 % ff2s = ffs < 0.01;
@@ -266,9 +268,6 @@ fclose(fidw);
 % subplot(5,1,4); plot(ff2); title('mask');
 % subplot(5,1,5); plot(dataMASK(:,6)); title('masked data');
 %%
-
-
-
 
 sprintf('finished file %d of %d files to process',i,length(listofbinaryfiles))
 cd .. %return to directory with other binary files
