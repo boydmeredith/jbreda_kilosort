ec3c6704bbd1c1b132bbdaf4d8d666f1a01615ce
jess-breda <jbreda19@gmail.com>
2020-07-30 18:17:09 -0400

    first step in preprocess on slurm

--- kilosort_preprocess_to_sort.sh
+++ kilosort_preprocess_to_sort.sh
@@ -1,8 +1,24 @@
-# copy and paste bash heading here, but add GPU extension (see Emily's cscope for more info
+#!/usr/bin/env bash
+#SBATCH --nodes=1                            # node count
+#SBATCH -o /jukebox/scratch/jbreda/ephys/W122/Kilosort_preprocess_testing/output.out  # where to save the output files of the job
+#SBATCH -e /jukebox/scratch/jbreda/ephys/W122/Kilosort_preprocess_testing/error.err   # where to save the error files of the job
+#SBATCH -t 1440                               # 24 hour time limit
+#SBATCH --mem=64000 # 64GB of RAM
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jbreda@princeton.edu
+#SBATCH --cpus-per-task=11                   # 11 cores requested
+#SBATCH --partition=Brody                    # run on brodylab parition
 
-# need to figure out bundle organization
+# hard coding input folder for git documentation, this should be where your raw .bin bundles are
+input_folder="/jukebox/scratch/jbreda/ephys/W122/Kilosort_preprocess_testing"
 
-# preprocess data, will create a new folder
-# add that folder to the path
+# repo should be in input_folder, grab its name to add path in matlab
+repo="Brody_Lab_Ephys"
+echo $repo
 
-# run kilosort form that folder, have kilosort take config file from somewhere else rather than one config per .bin
+
+# open matlab
+module load matlab/R2019b5
+
+# call kilosort_preprocess_forcluster_wrapper
+	matlab -nosplash -nodisplay -nodesktop -r "kilosort_preprocess_forcluster_wrapper('${input_folder}','${repo}');exit"
