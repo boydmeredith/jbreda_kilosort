d2ddaae8df2fe8ab640ba0a058291a6dbae095e7
jess-breda <jbreda19@gmail.com>
2020-07-31 14:53:51 -0400

    jobid 19754267 preprocessing W122 bundles

--- kilosort_preprocess_to_sort.sh
+++ kilosort_preprocess_to_sort.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 #SBATCH --nodes=1                            # node count
-#SBATCH -o /jukebox/scratch/jbreda/ephys/W122/Kilosort_preprocess_testing/output.out  # where to save the output files of the job
-#SBATCH -e /jukebox/scratch/jbreda/ephys/W122/Kilosort_preprocess_testing/error.err   # where to save the error files of the job
+#SBATCH -o /jukebox/scratch/jbreda/ephys/W122/kilosort_preprocess.out   # where to save the output files of the job
+#SBATCH -e /jukebox/scratch/jbreda/ephys/W122/kilosort_preprocess.err   # where to save the error files of the job
 #SBATCH -t 1440                               # 24 hour time limit
-#SBATCH --mem=64000 # 64GB of RAM
+#SBATCH --mem=84000 # 84GB of RAM
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jbreda@princeton.edu
 #SBATCH --cpus-per-task=11                   # 11 cores requested
 #SBATCH --partition=Brody                    # run on brodylab parition
 
 # hard coding input folder for git documentation, this should be where your raw .bin bundles are
-input_folder="/jukebox/scratch/jbreda/ephys/W122/Kilosort_preprocess_testing"
+input_folder="/jukebox/scratch/jbreda/ephys/W122/binfilesforkilosort2_19523713"
 
 # repo should be in input_folder, grab its name to add path in matlab, note that this function is called from the repo, so this your cwd
 
