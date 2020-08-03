c775e64829e538ceb12a5e319ee58dfbb92deffb
jess-breda <jbreda19@gmail.com>
2020-08-03 13:48:15 -0400

    wrapper fx outline for cluster

---
+++ utils/cluster_kilosort/main_kilosort_forcluster_wrapper.m
@@ -0,0 +1,11 @@
+function main_kilosort_forcluster_wrapper(input_folder, config_folder
+
+% paths to add
+% main rep
+% utils with npy, KS git submodule, KS cluster
+% configs
+% where the raw data is
+
+% call main_kilosort_fx_cluster(input_folder, config_folder
+
+end
