d3042b1c95eac709b2a60e8c9e53485aa2591e56
jess-breda <jbreda19@gmail.com>
2020-08-04 10:26:46 -0400

    uncommenting kilosort fx

--- utils/cluster_kilosort/main_kilosort_forcluster_wrapper.m
+++ utils/cluster_kilosort/main_kilosort_forcluster_wrapper.m
@@ -41,15 +41,9 @@ repo_and_subfolders = genpath(repo_folder); %where everything else is (including
 addpath(repo_and_subfolders);
 
 
-pwd
-% get into folder with main_kilsort_fx_cluster (conveniently also where I keep config
-% files)
-cd(config_folder)
-
-pwd
-
 disp('Passing into Kilosort')
-% call main_kilosort_fx_cluster(input_folder, config_folder)
+% call main_kilosort_fx
+main_kilosort_fx_cluster(input_folder, config_folder)
 disp('Kilosort Completed Successfully')
 
 end
