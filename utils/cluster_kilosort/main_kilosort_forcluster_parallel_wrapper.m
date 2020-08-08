b23d3d77ee3580847940bd22bf9ba8d82875bebe
jess-breda <jbreda19@gmail.com>
2020-08-08 17:31:35 -0400

    adding parrallel fx to make full file from array number

--- utils/cluster_kilosort/main_kilosort_forcluster_parallel_wrapper.m
+++ utils/cluster_kilosort/main_kilosort_forcluster_parallel_wrapper.m
@@ -28,21 +28,29 @@
 %('${input_folder}','${config_folder}','${repo_folder}');exit"
 % ---------------------
 %%
-function main_kilosort_forcluster_wrapper(job_folder, input_folders, config_folder, repo_folder, start_time)
+function main_kilosort_forcluster_parallel_wrapper(job_folder, input_base_path, config_path, repo_path, start_time)
 
 % printing test for cluster
-fprintf(input_folders)
-fprintf(config_folder)
-fprintf(repo_folder)
+fprintf(input_base_path)
+fprintf(config_path)
+fprintf(repo_path)
 fprintf(job_folder)
 
 
 % generate folder to process
-(fullfile(input_folders, delim, job_folder)))
+if ispc
+    delim='\';
+else
+    delim='/';
+end
+
+input_path = (fullfile(input_base_path, delim, job_folder));
+
+fprintf(input_path)
 
 % add paths
-addpath(input_folder); %where bin file is
-repo_and_subfolders = genpath(repo_folder); %where everything else is (including config files)
+addpath(input_path); %where bin file is
+repo_and_subfolders = genpath(repo_path); %where everything else is (including config files)
 addpath(repo_and_subfolders);
 
 
