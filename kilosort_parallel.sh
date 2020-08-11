63f5faf2a8dd1f2246373b0f52886d1558a09141
jess-breda <jbreda19@gmail.com>
2020-08-11 16:41:34 -0400

    jobid 5106101 W122 big run, array 0-121, had to ask for logner run time to get job allocation

--- kilosort_parallel.sh
+++ kilosort_parallel.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks-per-socket=1
 #SBATCH --gres=gpu:1
 #SBATCH --mem=10000          # 10 GB RAM 
-#SBATCH -t 20                # time (minutes)
+#SBATCH -t 360                # time (minutes)
 #SBATCH -o /scratch/gpfs/jbreda/ephys/kilosort/W122/logs/output_%a_%j.out
 #SBATCH -e /scratch/gpfs/jbreda/ephys/kilosort/W122/logs/error_%a_%j.err
 
